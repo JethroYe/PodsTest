f66033d7d8cc376e74d8dda0c741d168431212b4
叶宇轩 <yeyuxuan@yeyuxuandeMacBook-Pro.local>
2016-11-27 10:49:06 +0800

    update

---
+++ PodsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodsTest
+//
+//  Created by Mac on 2016/11/27.
+//  Copyright © 2016年 Jethro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PodsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PodsTest
+//
+//  Created by Mac on 2016/11/27.
+//  Copyright © 2016年 Jethro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
